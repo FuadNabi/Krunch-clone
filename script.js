416e49d44e26b68e32af24632e876a8b6852d2b3
8Bts <mcmizze@yahoo.com>
2022-02-05 17:16:55 +0400

    Add console.log to script

--- script.js
+++ script.js
@@ -7,21 +7,22 @@ let toggle = "deqiq-olsun";
 downloadBtn.onclick = () => {
   if ( toggle == "deqiq-olsun" ) {
     downloadBtn.style.backgroundColor = 'red';
-    toggle = "onda-iiiii"
+    toggle = "onda-iiiii";
   } else {
     downloadBtn.style.backgroundColor = 'green';
-    toggle = "deqiq-olsun"
+    toggle = "deqiq-olsun";
   }
 }
 
 viewFeaturesdBtn.onclick = () => {
   if ( toggle == "deqiq-olsun" ) {
     viewFeaturesdBtn.style.backgroundColor = 'orange';
-    toggle = "onda-iiiii"
+    toggle = "onda-iiiii";
   } else {
     viewFeaturesdBtn.style.backgroundColor = 'purple';
-    toggle = "deqiq-olsun"
+    toggle = "deqiq-olsun";
   }
 
 }
 
+console.log('Hello');
